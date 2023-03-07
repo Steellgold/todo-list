277bb27b3ee275f6cbce8f7557704e2dfd1813e0
Gaëtan H <steellgold0@gmail.com>
2023-03-07 16:33:23 +0100

    📦 DayJS, Tailwind Forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,12 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./lib/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/forms")]
+};
